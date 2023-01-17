ddf602a379efbaaf9c7779dd5a4a1eda6c93cf9c
Boboye-Ak <boboye771@gmail.com>
2023-01-17 19:11:41 +0100

    styled the about page a bit

--- components/Sections/AboutSection.js
+++ components/Sections/AboutSection.js
@@ -1,5 +1,44 @@
-const AboutSection = ({ activePage }) => {
-    return <div className={`section about ${activePage != "about" && "hidden"}`}>AboutSection</div>
+import { Icon } from "@iconify/react"
+import { useState } from "react"
+
+const AboutSection = ({ activePage, setActivePage }) => {
+    const [showArrow, setShowArrow] = useState(true)
+    return (
+        <div className={`section about ${activePage != "about" && "hidden"}`}>
+            <div className="upper-third">
+                {" "}
+                <div
+                    className={`down-arrow ${!showArrow && "hidden"}`}
+                    onClick={() => {
+                        setActivePage("home")
+                    }}
+                >
+                    <Icon icon="ic:outline-arrow-circle-up" />
+                </div>
+            </div>
+            <div className="middle-third">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+                the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+                with desktop publishing software like Aldus PageMaker including versions of Lorem
+                Ipsum
+            </div>
+            <div className="lower-third">
+                {" "}
+                <div
+                    className={`down-arrow ${!showArrow && "hidden"}`}
+                    onClick={() => {
+                        setActivePage("resume")
+                    }}
+                >
+                    <Icon icon="ic:outline-arrow-circle-down" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default AboutSection
--- components/Sections/HomeSection.js
+++ components/Sections/HomeSection.js
@@ -1,17 +1,32 @@
 import { Icon } from "@iconify/react"
+import { useState } from "react"
+import Typewriter from "typewriter-effect"
 
 const HomeSection = ({ activePage, setActivePage }) => {
+    const [showArrow, setShowArrow] = useState(false)
+    const handleTypewriter = (typewriter) => {
+        typewriter
+            .pauseFor(500)
+            .typeString("I am a FullStack Web2/Web3 Developer")
+            .callFunction(() => {
+                setShowArrow(true)
+            })
+            .start()
+    }
+
     return (
         <div className={`section home ${activePage != "home" && "hidden"}`}>
             <div className="upper-half">
                 <div className="name">
                     Akinboboye <span>Akinwande</span>
                 </div>
-                <div className="title">I am a FullStack Web2/Web3 Developer</div>
+                <div className="title">
+                    <Typewriter onInit={handleTypewriter} />
+                </div>
             </div>
             <div className="bottom-half">
                 <div
-                    className="down-arrow"
+                    className={`down-arrow ${!showArrow&&"hidden"}`}
                     onClick={() => {
                         setActivePage("about")
                     }}
