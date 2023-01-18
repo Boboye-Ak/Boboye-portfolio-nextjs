05961acc2fdef24c1c9d3945924ef36fb6cf5e80
Boboye-Ak <boboye771@gmail.com>
2023-01-18 11:43:59 +0100

    started working on resume section

--- components/Sections/ResumeSection.js
+++ components/Sections/ResumeSection.js
@@ -1,5 +1,37 @@
-const ResumeSection = ({ activePage }) => {
-    return <div className={`section resume ${activePage != "resume" && "hidden"}`}>ResumeSection</div>
+import { Icon } from "@iconify/react"
+import { useState } from "react"
+
+const ResumeSection = ({ activePage, setActivePage }) => {
+    const [showArrow, setShowArrow] = useState(true)
+    return (
+        <div className={`section resume ${activePage != "resume" && "hidden"}`}>
+            <div className="upper-third">
+                {" "}
+                <div
+                    className={`down-arrow ${!showArrow && "hidden"}`}
+                    onClick={() => {
+                        setActivePage("about")
+                    }}
+                >
+                    <Icon icon="ic:outline-arrow-circle-up" />
+                </div>
+            </div>
+            <div className="middle-third">
+                <div className="resume-page">Resume Page</div>
+            </div>
+            <div className="lower-third">
+                {" "}
+                <div
+                    className={`down-arrow ${!showArrow && "hidden"}`}
+                    onClick={() => {
+                        setActivePage("projects")
+                    }}
+                >
+                    <Icon icon="ic:outline-arrow-circle-down" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ResumeSection
