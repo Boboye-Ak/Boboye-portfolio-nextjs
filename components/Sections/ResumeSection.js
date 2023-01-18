cbef6c3e9be6292879648a0dd4e271921747eaf0
Boboye-Ak <boboye771@gmail.com>
2023-01-18 15:27:13 +0100

    created mobile sidebar and resume page

--- components/Sections/ResumeSection.js
+++ components/Sections/ResumeSection.js
@@ -1,8 +1,34 @@
 import { Icon } from "@iconify/react"
 import { useState } from "react"
 
+const resumePagesImages = [
+    "resume pages/Akinboboye Akinwande Resume_01.jpg",
+    "resume pages/Akinboboye Akinwande Resume_02.jpg",
+    "resume pages/Akinboboye Akinwande Resume_03.jpg",
+]
+
 const ResumeSection = ({ activePage, setActivePage }) => {
     const [showArrow, setShowArrow] = useState(true)
+    const [resumePageIndex, setResumePageIndex] = useState(0)
+
+    const nextPage = () => {
+        setResumePageIndex((currentPageIndex) => {
+            if (currentPageIndex < resumePagesImages.length - 1) {
+                return currentPageIndex + 1
+            } else {
+                return currentPageIndex
+            }
+        })
+    }
+    const previousPage = () => {
+        setResumePageIndex((currentPageIndex) => {
+            if (currentPageIndex == 0) {
+                return currentPageIndex
+            } else {
+                return currentPageIndex - 1
+            }
+        })
+    }
     return (
         <div className={`section resume ${activePage != "resume" && "hidden"}`}>
             <div className="upper-third">
@@ -17,7 +43,35 @@ const ResumeSection = ({ activePage, setActivePage }) => {
                 </div>
             </div>
             <div className="middle-third">
-                <div className="resume-page">Resume Page</div>
+                <div className="resume-page">
+                    <a
+                        className="download-resume"
+                        href="Akinboboye Akinwande Resume.pdf"
+                        download={true}
+                    >
+                        <Icon icon="material-symbols:download" />
+                    </a>
+                    <div className="page-label">
+                        {resumePageIndex + 1}/{resumePagesImages?.length}
+                    </div>
+                    <img src={resumePagesImages[resumePageIndex]} />
+                    <div
+                        className="change-resume-page before"
+                        onClick={() => {
+                            previousPage()
+                        }}
+                    >
+                        <Icon icon="material-symbols:navigate-before" />
+                    </div>
+                    <div
+                        className="change-resume-page next"
+                        onClick={() => {
+                            nextPage()
+                        }}
+                    >
+                        <Icon icon="material-symbols:navigate-next" />
+                    </div>
+                </div>
             </div>
             <div className="lower-third">
                 {" "}
--- components/Sidebar/SideBarMobile.js
+++ components/Sidebar/SideBarMobile.js
@@ -1,5 +1,100 @@
+import { Icon } from "@iconify/react"
+import { useState } from "react"
+
 const SideBarMobile = ({ activePage, setActivePage }) => {
-    return <div className="sidebar-mobile">SideBarMobile</div>
+    const [showSideBar, setShowSideBar] = useState(false)
+    return (
+        <>
+            <div
+                className="menu-button"
+                onClick={() => {
+                    setShowSideBar(!showSideBar)
+                }}
+            >
+                {!showSideBar ? (
+                    <Icon icon="material-symbols:menu" />
+                ) : (
+                    <Icon icon="ri:close-fill" />
+                )}
+            </div>
+            <div className={`sidebar-mobile ${!showSideBar && "hidden"}`}>
+                <div className="profile-picture">
+                    <img src="/developer-image.jpeg" />
+                    <div className="label">Akinboboye Akinwande</div>
+                </div>
+                <div className="page-list">
+                    <div
+                        className={`page ${activePage == "home" && "active"}`}
+                        onClick={() => {
+                            setActivePage("home")
+                            setShowSideBar(false)
+                        }}
+                    >
+                        <Icon icon="material-symbols:home-outline" />
+                        Home
+                    </div>
+                    <div
+                        className={`page ${activePage == "about" && "active"}`}
+                        onClick={() => {
+                            setActivePage("about")
+                            setShowSideBar(false)
+                        }}
+                    >
+                        <Icon icon="healthicons:ui-user-profile-outline" />
+                        {"  "}
+                        About
+                    </div>
+                    <div
+                        className={`page ${activePage == "resume" && "active"}`}
+                        onClick={() => {
+                            setActivePage("resume")
+                            setShowSideBar(false)
+                        }}
+                    >
+                        <Icon icon="mdi:resume" />
+                        {"  "}
+                        Resume
+                    </div>
+                    <div
+                        className={`page ${activePage == "projects" && "active"}`}
+                        onClick={() => {
+                            setActivePage("projects")
+                            setShowSideBar(false)
+                        }}
+                    >
+                        <Icon icon="bytesize:portfolio" />
+                        {"  "}
+                        Projects
+                    </div>
+                    <div
+                        className={`page ${activePage == "contact" && "active"}`}
+                        onClick={() => {
+                            setActivePage("contact")
+                            setShowSideBar(false)
+                        }}
+                    >
+                        <Icon icon="material-symbols:mail-outline-sharp" />
+                        {"  "}
+                        Contact
+                    </div>
+                </div>
+                <div className="socials-list">
+                    <a>
+                        <Icon icon="bi:github" />
+                    </a>
+                    <a>
+                        <Icon icon="bi:linkedin" />
+                    </a>
+                    <a>
+                        <Icon icon="bi:twitter" />
+                    </a>
+                    <a>
+                        <Icon icon="mdi:gmail" />
+                    </a>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default SideBarMobile
